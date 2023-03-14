b146b5894878fe3e229ee77971ea84bf0a53ed36
Kollin Francis <kollinzcfrancis@gmail.com>
2023-03-13 20:39:13 -0400

    Truncating insertions to only jōyō kanji

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ WIP project, please don't use :)
 
 The end goal of this is to be able to easily create a SQL db structured around kanjidic2. There are a couple projects like this, but I wanted to try my hand at it to learn a little bit of Rust. I also will eventually use it for a side project.
 
+Because of the above, the Rust is pretty poor I'm sure. If you come across this and are a Rust wizard please feel free to make a PR and offer advice.
+
 For a project that seems to be fully working see: [edict_database](https://github.com/odrevet/edict_database)
 
 My goals with the project are as follows:
-* [ ] Parse kanjidic xml file into structs
-* [ ] Create SQL script that will create all needed tables, structure TBD honestly
+* [*] Parse kanjidic xml file into structs
+* [*] Create SQL script that will create all needed tables, structure TBD honestly
   * Going with a script instead of creating the DB and everything because I don't like migrating from DB to DB. I want a file that I can copy the contents of and then run against any DB I plan on using. I don't want to do like `SHOW CREATE TABLE` and copy that then run against my own db or anything.
 * [ ] Maybe offer a way to give a connection string to have the script auto run?
-* [ ] Download latest kanjidic file for you and unzip it
+* [*] Download latest kanjidic file for you and unzip it
 * [ ] Provide command line arguments to pick and choose which properties you want to parse
   * Maybe command line arguments for different SQL syntaxes as well
 * [ ] Maybe get working with JM Dict and generate a vocabulary table and such as well
 * [ ] Learn Rust :)
+
+Currently this will output a large creation script of all kanji with meanings, readings, and less than grade 8. Basically all the jōyō kanji.
 
 Honestly a little overkill for something that'll likely be used one time by anyone else who uses it, but 🤷🏼‍♂️🤷🏼‍♂️
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
-use std::time::Instant;
 use std::{fs};
 use std::fmt::Write;
-use std::io::Write as OtherWrite;
+use std::io::{Write as OtherWrite};
+use flate2::read::GzDecoder;
+use std::io::Read;
 
 use dictionary::Dictionary;
 mod dictionary;
@@ -12,56 +13,66 @@ fn get_result(content: String) -> std::string::String {
   let mut res = String::new(); 
 
   for c in doc.character {
-    writeln!(&mut res, "INSERT INTO kanji_db.kanji (literal) VALUES ('{}');", c.literal).unwrap();
+    let mut insert_statements = String::new();
+    let mut has_meanings = false;
+    let mut has_readings = false;
+    if let Some(rm) = c.reading_meaning {
+      writeln!(&mut insert_statements, "INSERT INTO kanji_db.kanji (literal) VALUES ('{}');", c.literal).unwrap();
 
-    for cp in c.codepoint.values {
-      writeln!(&mut res, "INSERT INTO kanji_db.codepoint (code_type, code, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", cp.code_type, cp.code, c.literal).unwrap();
-      // codepoints.insert(c.literal.clone(), cp);
-    }
+      for cp in c.codepoint.values {
+        writeln!(&mut insert_statements, "INSERT INTO kanji_db.codepoint (code_type, code_value, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", cp.code_type, cp.code, c.literal).unwrap();
+      }
+  
+      for rad in c.radical.rad_value {
+        writeln!(&mut insert_statements, "INSERT INTO kanji_db.radical (radical_type, radical_id, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", rad.rad_type, rad.id, c.literal).unwrap();
+      }
 
-    for rad in c.radical.rad_value {
-      writeln!(&mut res, "INSERT INTO kanji_db.radical (radical_type, radical_id, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", rad.rad_type, rad.id, c.literal).unwrap();
-    }
-
-    match c.reading_meaning {
-      Some(x) => {
-        for group in x.rm_group {
-          match group.meaning {
-            Some(g) => {
-              for meaning in g {
-                match meaning.m_lang {
-                  Some(lang) => {
-                    writeln!(&mut res, "INSERT INTO kanji_db.meaning (meaning, lang, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", meaning.meaning.replace("'", "''"), lang, c.literal).unwrap();
-                  }
-                  None => {
-                    writeln!(&mut res, "INSERT INTO kanji_db.meaning (meaning, lang, kanji_id) VALUES('{}', NULL, (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", meaning.meaning.replace("'", "''"), c.literal).unwrap();
-                  }
+      for group in rm.rm_group {
+        match group.meaning {
+          Some(g) => {
+            for meaning in g {
+              match meaning.m_lang {
+                Some(lang) => {
+                  has_meanings = true;
+                  writeln!(&mut insert_statements, "INSERT INTO kanji_db.meaning (meaning, lang, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", meaning.meaning.replace("'", "''"), lang, c.literal).unwrap();
+                }
+                None => {
+                  writeln!(&mut insert_statements, "INSERT INTO kanji_db.meaning (meaning, lang, kanji_id) VALUES('{}', NULL, (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", meaning.meaning.replace("'", "''"), c.literal).unwrap();
                 }
               }
-            }
-            None => ()
-          }
-          match group.reading {
-            Some(g) => {
-              for reading in g {
-                writeln!(&mut res, "INSERT INTO kanji_db.reading (reading, reading_type, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", reading.reading.replace("'", "''"), reading.r_type, c.literal).unwrap();
-              }
-            }
-            None => ()
-          }
-        }
-        match x.nanori {
-          Some(nanoris) => {
-            for nanori in nanoris {
-              writeln!(&mut res, "INSERT INTO kanji_db.reading (reading, reading_type, kanji_id) VALUES('{}', 'nanori', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", nanori, c.literal).unwrap();
             }
           }
           None => ()
         }
+        match group.reading {
+          Some(g) => {
+            for reading in g {
+              has_readings = true;
+              writeln!(&mut insert_statements, "INSERT INTO kanji_db.reading (reading, reading_type, kanji_id) VALUES('{}', '{}', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", reading.reading.replace("'", "''"), reading.r_type, c.literal).unwrap();
+            }
+          }
+          None => ()
+        }
+        
       }
-      None => ()
+
+      match rm.nanori {
+        Some(nanoris) => {
+          for nanori in nanoris {
+            writeln!(&mut insert_statements, "INSERT INTO kanji_db.reading (reading, reading_type, kanji_id) VALUES('{}', 'nanori', (SELECT id FROM kanji_db.kanji WHERE literal = '{}'));", nanori, c.literal).unwrap();
+          }
+        }
+        None => ()
+      }
+
+      if let Some(grade) = c.misc.grade {
+        if has_meanings && has_readings && grade < 9{
+          write!(&mut res, "{}", insert_statements).unwrap();
+        }
+      }
     }
   }
+    
   return res;
 }
 
@@ -72,14 +83,12 @@ fn write_result(result_sql: String) {
   write!(output, "{}", other_content).unwrap();
 }
 
-fn main() {
-  let file_name: &str = "./kanjidic2.xml";
-  let content: String = fs::read_to_string(file_name).expect("Unable to read file");
-  
-  let now = Instant::now();
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
+  let resp = reqwest::blocking::get("http://www.edrdg.org/kanjidic/kanjidic2.xml.gz")?;
+  let mut d = GzDecoder::new(resp);
+  let mut content = String::new();
+  d.read_to_string(&mut content).unwrap();
 
   write_result(get_result(content));
-  
-  let elapsed_time = now.elapsed();
-  println!("Ran in {} seconds", elapsed_time.as_secs());
+  Ok(())
 }
